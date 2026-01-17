d388d3ff901654116bb345591a96bbd7723f79ca
mVitoros <mixailvitoros@gmail.com>
2026-01-17 20:49:02 +1100

    removed mocks, setup prisma models, get products from postgres

--- app/ProductCard.tsx
+++ app/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { Product } from "@/lib/mocks";
+import { Product } from "@/generated/prisma/client";
 import { formatPrice } from "@/lib/utils";
 import Image from "next/image";
 
@@ -6,13 +6,15 @@ export function ProductCard({ product }: { product: Product }) {
   return (
     <div className="border border-gray-200 rounded-lg p-4">
       <div className="relative aspect-video">
-        <Image
-          src={product.image}
-          alt={product.name}
-          className="object-cover"
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          fill
-        />
+        {product.image && (
+          <Image
+            src={product.image}
+            alt={product.name}
+            className="object-cover"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            fill
+          />
+        )}
       </div>
       <h2 className="text-lg font-semibold">{product.name}</h2>
       <p className="text-gray-600">{formatPrice(product.price)}</p>
