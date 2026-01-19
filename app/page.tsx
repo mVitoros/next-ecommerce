f0c6d5a821003deda2caf6eed3c3bc3cf9a0c245
mVitoros <mixailvitoros@gmail.com>
2026-01-19 18:08:58 +1100

    added navbar, breadcrumbs, and theme selection

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ import { prisma } from "@/lib/prisma";
 import { Suspense } from "react";
 import ProductsSkeleton from "./ProductsSkeleton";
 import { sleep } from "@/lib/utils";
+import { Breadcrumbs } from "@/components/breadcrumbs";
 
 type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 
@@ -48,8 +49,8 @@ export default async function HomePage(props: { searchParams: SearchParams }) {
   const totalPages = Math.ceil(total / pageSize);
 
   return (
-    <main className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-6">Home</h1>
+    <main className="container mx-auto py-4">
+      <Breadcrumbs items={[{ label: "Products", href: "/" }]} />
 
       <Suspense key={page} fallback={<ProductsSkeleton />}>
         <Products page={page} />
